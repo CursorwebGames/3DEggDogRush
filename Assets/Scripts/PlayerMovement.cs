cb98201c89e66a921dc2b8e1ac35c8c7fb6f2c4d
Junhao Zhang <junhaodev@gmail.com>
2021-05-03 20:57:26 -0500

    Made movement better

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,30 +4,21 @@ public class PlayerMovement : MonoBehaviour
 {
     public bool grounded = true;
 
+    public float moveSpeed;
     public float laneWidth;
-    private int offset = 0;
 
     public Rigidbody rb;
     public float jumpHeight;
 
     private void Update()
     {
-        bool left = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
-        bool right = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
+        float horiz = Input.GetAxis("Horizontal");
 
-        // todo: animate?
-        if (left && offset != -1)
-        {
-            offset--;
-            transform.position -= new Vector3(laneWidth, 0, 0);
-        }
-        else if (right && offset != 1)
-        {
-            offset++;
-            transform.position += new Vector3(laneWidth, 0, 0);
-        }
+        rb.MovePosition(transform.position + new Vector3(horiz, 0, 0) * moveSpeed * Time.deltaTime);
 
-        if (grounded && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -laneWidth, laneWidth), transform.position.y, transform.position.z);
+
+        if (grounded && Input.GetAxis("Vertical") > 0)
         {
             grounded = false;
             rb.AddForce(0, jumpHeight, 0);
