f8121175d7e5480ff952f06d6a53e4d0e7f1934b
Junhao Zhang <junhaodev@gmail.com>
2021-05-04 21:18:40 -0500

    Added Chunk Spawner

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 
 // This will be used to abstract level chunks and make chunks.
@@ -11,12 +12,37 @@ public class LevelManager : MonoBehaviour
     public int score = 0;
     public Dictionary<BiomeType, GameObject[]> levelChunks = new Dictionary<BiomeType, GameObject[]>();
 
+    public Vector3 offset;
+
+    public float tick = 0;
+    public float maxTick;
+
+    private GameManager gameManager;
+    private BiomeType biome;
+
     private void Start()
     {
         foreach (string biome in Enum.GetNames(typeof(BiomeType)))
         {
             BiomeType biomeEnum = (BiomeType)Enum.Parse(typeof(BiomeType), biome);
-            levelChunks.Add(biomeEnum, GameObject.FindGameObjectsWithTag($"{biome}Chunk"));
+            levelChunks.Add(biomeEnum, Resources.LoadAll<GameObject>(biome));
+        }
+
+        gameManager = FindObjectOfType<GameManager>();
+        biome = gameManager.biome;
+    }
+
+    private void Update()
+    {
+        GameObject[] chunkArr = levelChunks[biome];
+
+        tick += Time.deltaTime;
+        if (tick > maxTick)
+        {
+            tick = 0;
+            int index = UnityEngine.Random.Range(0, chunkArr.Length - 1);
+            GameObject prefab = chunkArr[index];
+            Instantiate(prefab, offset, Quaternion.identity);
         }
     }
 }
